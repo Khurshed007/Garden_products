ed1dfe843df1c9e009ce56a605a801ff0bc60a30
Khurshed007 <khurshed.ahmadjonov@bk.ru>
2024-06-04 11:46:21 +0200

    hook created and request

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,4 +1,15 @@
 export const getCategorys = (state) => state.shop.category; // 5 CategoryItems
 
+export const getDiscountItems = (state) => {
+  const items = [...state.shop.items];
 
+  return items
+  // .sort(() => Math.random() - 0.5) // Перемешиваем элементы в случайном порядке
+  .sort(
+    (elem, elem2) =>
+      getDiscountPercent(elem2.price, elem2.discont_price) -
+      getDiscountPercent(elem.price, elem.discont_price)
+  )
+  .filter(({ discont_price }, index) => discont_price !== null && index < 4);
+};
 
--- src/views/main/index.jsx
+++ src/views/main/index.jsx
@@ -1,20 +1,32 @@
 import React, {useEffect} from "react";
-
+import { CardItem } from "../../components/card-item/card-item";
 import { Head } from "./head/head";
-
+import { Categories } from "../categories";
 
 // Redux
-
+import { requestAllProductItem } from "../../store/async-action";
+import { getDiscountItems } from "../../store/selectors";
 // Кастомный хук
+import useData from "../../hooks/useData";
+import {useDispatch } from "react-redux";
 
 
 export const Main = () => {
+  const DATA_DISCOUNT_ITEMS = useData(requestAllProductItem, getDiscountItems);
 
 
   return   (
     <>
       <Head />
- 
+      <section>
+        <CardItem
+          dataItems={DATA_DISCOUNT_ITEMS}
+          text={"Sales"}
+          btnText={"All Sales"}
+          btnDispplay={true}
+          hideBreadCrumbs = {true}
+        />
+      </section>
     </>
   );
 };
