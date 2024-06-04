97d6f70158ed0a418bcc7c6e06afdec9aba64df5
Khurshed007 <khurshed.ahmadjonov@bk.ru>
2024-06-04 16:11:00 +0200

    route is done

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,20 +3,24 @@ import { Routes, Route } from "react-router-dom";
 import { Categories } from "./views/categories";
 import { Main } from "./views/main";
 import { Layout } from "./views/layout";
+import { NotFound } from "./views/not-found";
+import CartContent from "./components/cart-content/cart-content";
+import { Product } from "./views/product/product";
 import { AllProductsRoute } from "./views/all-products/route";
 import { AllSalesRoute } from "./views/all-sales/route";
-import { NotFound } from "./views/not-found";
-import { Product } from "./views/product/product";
+import { LikedProductsRoute } from "./views/liked-products/liked-products-route";
 export const RootRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Main />} />
         <Route path="categories/*" element={<Categories />} />
         <Route path="all-sales/*" element={<AllSalesRoute />} />
         <Route path="all-products/*" element={<AllProductsRoute />} />
+        <Route path="liked-products/*" element={<LikedProductsRoute />} />
         <Route path="/product/:productId" element={<Product />} />
-        <Route path="*" element={<NotFound/>} />
+        <Route path="cart" element={<CartContent />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
