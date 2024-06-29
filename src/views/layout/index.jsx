64764ef11d57236ce2b2d0c3a228ff5f7df785d6
Khurshed007 <khurshed.ahmadjonov@bk.ru>
2024-06-29 19:11:49 +0200

    all changed

--- src/views/layout/index.jsx
+++ src/views/layout/index.jsx
@@ -2,25 +2,27 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { Footer } from "../footer/footer";
 import { Navigation } from "../navigation/navigation";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { setPath } from "../../store/shop-slice";
 import { getPath } from "../../utils/getPath";
 import { Loading } from "../loading/loading";
-import { useShopAction } from "../../hooks/useShopAction";
 import { ErrorMessage } from "../404/error";
+import { getAllItems, getCategorys } from "../../store/selectors";
 export const Layout = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const { pathname } = location;
-  const  {category,items,isLoading,isError} = useSelector((state) => state.shop);
+  const category = useSelector(getCategorys)
+  const items = useSelector(getAllItems)
+  const isLoading = useSelector((state) => state.shop.isLoading)
+  const  isError = useSelector((state) => state.shop.isError);
 
   useEffect(() => {
     dispatch(setPath(getPath(pathname, items, category)));
   }, [pathname, category, items, dispatch]);
 
-  const myPath = useSelector((state) => state.shop.path);
   return (
     <main>
       <Navigation />
